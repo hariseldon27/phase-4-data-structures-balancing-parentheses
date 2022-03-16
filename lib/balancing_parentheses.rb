ac1defa315cc8c43928bd8bd4c56069092867370
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-16 15:31:54 -0700

    commit tree c136b773a2aa5e71f2b039efa68fe4b3676ea9d0

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,16 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(str)
+    opener = 0
+    closer = 0
+    str.chars.each do |letter|
+        if letter == "("
+            opener + 1
+        else 
+            closer + 1 
+        end
+        puts "Opener: " + opener
+        puts "Closer: " + closer 
+        return closer - opener
+end
